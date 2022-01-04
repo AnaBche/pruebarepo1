666a5fef04a1033c9898f89e338fae1294a2aeca
AnaBche <anavolpe3@gmail.com>
2022-01-04 14:28:09 -0300

    esUnaPruebaDeUnaPrueba

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,16 @@
+
+
+package hola.mundo;
+
+
+public class HolaMundo {
+
+   
+    public static void main(String[] args) {    
+    
+        System.out.println("hola, mundo");
+System.out.println("que huevada");
+
+    }
+
+}
